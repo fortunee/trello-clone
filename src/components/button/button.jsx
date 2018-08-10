e6569a8344343f85859ccafbc5fd40667c647851
Mbarak Mbigo <mbarak.mbigo@andela.com>
2018-08-10 14:48:01 +0300

    chore: setup styled components  - install and configure styled-components  - refactor app to use styled components

---
+++ src/components/button/button.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+const Button = styled.button`
+  height: 2rem;
+  outline: none;
+  background-color: #2b62ad;
+  color: #fff;
+  border: 2px solid #2b62ad;
+  border-radius: 3px;
+  transition: all 0.2s ease-in-out;
+
+  &: hover {
+    background-color: #424446;
+  }
+`;
+
+export default Button;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { injectGlobal } from "styled-components";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+
+injectGlobal`
+  body {
+    font-family: sans-serif;
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
